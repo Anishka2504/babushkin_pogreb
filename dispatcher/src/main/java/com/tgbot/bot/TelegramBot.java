c48c0ca5385a73ce76484e12cba74267f7ad84ae
Anna Drozdovskaya <hanna_drazdouskaya@senla.eu>
2024-10-29 17:20:14 +0300

    initial commit

---
+++ dispatcher/src/main/java/com/tgbot/bot/TelegramBot.java
@@ -0,0 +1,57 @@
+package com.tgbot.bot;
+
+import com.tgbot.controller.UpdateController;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+    private UpdateController updateController;
+
+    public TelegramBot(@Value("${bot.token}") String botToken, UpdateController updateController) {
+        super(botToken);
+        this.updateController = updateController;
+    }
+
+    @PostConstruct
+    public void init() {
+        updateController.registerBot(this);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var message = update.getMessage();
+        log.info("Received update: {}", message.getText());
+
+        var response = new SendMessage();
+        response.setChatId(message.getChatId().toString());
+        response.setText("Hello " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName());
+        sendAnswerMessage(response);
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+}
---
+++ dispatcher/src/main/java/com/tgbot/controller/UpdateController.java
@@ -0,0 +1,67 @@
+package com.tgbot.controller;
+
+import com.tgbot.bot.TelegramBot;
+import com.tgbot.utils.MessageUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@Slf4j
+public class UpdateController {
+
+    private TelegramBot telegramBot;
+    private MessageUtils messageUtils;
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Update is null");
+            return;
+        }
+
+        if (update.getMessage() != null) {
+            distributeMessageByType(update);
+        } else {
+            log.error("Received unsupported message type {}", update);
+        }
+    }
+
+    private void distributeMessageByType(Update update) {
+        var message = update.getMessage();
+        if (message.getText() != null) {
+            processTextMessage(update);
+        } else if (message.getDocument() != null) {
+            processDocMessage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "Unsupported message type");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+
+    }
+
+    private void processDocMessage(Update update) {
+
+    }
+
+    private void processTextMessage(Update update) {
+        
+    }
+}
