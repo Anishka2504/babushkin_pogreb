39ae943ad724559fef4de4b81939033a5151ffb1
Anna Drozdovskaya <hanna_drazdouskaya@senla.eu>
2024-11-06 13:27:35 +0300

    rabbitmq

--- dispatcher/src/main/java/com/tgbot/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/com/tgbot/service/impl/UpdateProducerImpl.java
@@ -1,16 +1,22 @@
 package com.tgbot.service.impl;
 
 import com.tgbot.service.UpdateProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class UpdateProducerImpl implements UpdateProducer {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
